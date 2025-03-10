da969ed0393536f9d0325de13f40591e49b339ef
zzasdf <zzasdf@sjtu.edu.cn>
2025-03-11 00:42:57 +0800

    rename

--- SSL/scripts/run_ssl.sh
+++ SSL/scripts/run_ssl.sh
@@ -1,15 +1,19 @@
 #! /usr/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
+# change --label to the train label you want to use
+# change --label-rate to 100 when you use the k-means of Fbank as target, in other cases it should be 50
+# change --exp-dir to the path you want to save checkpoints
+
 python zipformer_fbank/pretrain.py \
     --world-size 8 \
-    --num-epochs 400 \
+    --num-epochs 20 \
     --start-epoch 1 \
     --use-fp16 1 \
     --label-type kmeans_ASR_100h \
     --label-rate 50 \
     --sample-rate 100 \
-    --exp-dir zipformer_fbank/exp-kmeans_ASR_100h-all \
+    --exp-dir zipformer_fbank/exp \
     --max-duration 1000 \
     --train-cut large \
     --accum-grad 1 \
