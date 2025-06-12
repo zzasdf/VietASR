c7b6dc4a08bb9377a214fa00d326321e2b833d20
zzasdf <zzasdf@sjtu.edu.cn>
2025-06-13 00:42:12 +0800

    add segment merge to vad.py

--- SSL/local/vad.py
+++ SSL/local/vad.py
@@ -8,8 +8,21 @@ import multiprocessing as mp
 from argparse import ArgumentParser
 
 def routine(wav_file, tgt_dir, model, device, args):
-    speech, sample_rate = sf.read(wav_file)
+    try:
+        speech, sample_rate = sf.read(wav_file)
+    except KeyboardInterrupt:
+        raise
+    except:
+        return
+
+    if speech.shape[0]/sample_rate<=args.max_duration:
+        if speech.shape[0]/sample_rate<=args.min_duration:
+            return
+        os.system(f"cp {wav_file} {tgt_dir}.wav")
+        return
+
     os.makedirs(tgt_dir, exist_ok=True)
+
     if args.streaming:
         chunk_size = 200 # ms
         chunk_stride = int(chunk_size * sample_rate / 1000)
@@ -36,6 +49,41 @@ def routine(wav_file, tgt_dir, model, device, args):
     else:
         res = model.generate(input=wav_file, disable_pbar=True, device=device)
 
+    def merge_segment(segment, merge_time, min_time, max_blank_time):
+        new_value = []
+        start = None
+        end = None
+
+        if merge_time is None:
+            return segment
+
+        for item in segment:
+            start_time = item[0]/1000
+            end_time = item[1]/1000
+            if start is None:
+                start = item[0]
+                end = item[1]
+            else:
+                if end/1000 - start/1000> merge_time:
+                    new_value.append([start, end])
+                    start = item[0]
+                    end = item[1]
+                elif start_time - end/1000 > max_blank_time:
+                    # not merget when there is too much blank
+                    if end/1000 - start/1000> min_time:
+                        new_value.append([start, end])
+                    start = item[0]
+                    end = item[1]
+                else:
+                    end = item[1]
+
+        if start is not None and end/1000 - start/1000> min_time:
+            new_value.append([start, end])
+
+        return new_value
+
+    res[0]["value"] = merge_segment(res[0]["value"], args.merge_duration, args.min_duration, args.max_blank_duration)
+
 
     if args.max_duration is not None:
         max_size = int(args.max_duration*sample_rate)
@@ -59,12 +107,12 @@ def routine(wav_file, tgt_dir, model, device, args):
 def main(rank, args, task_lines):
     task_dir = args.task_dir
     num_gpus = torch.cuda.device_count()
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(rank%num_gpus)
+    # os.environ["CUDA_VISIBLE_DEVICES"] = str(rank%num_gpus)
     world_size = args.world_size
     from funasr import AutoModel
 
-    device = torch.device("cuda:0")
-    model = AutoModel(model="fsmn-vad", model_revision="v2.0.4", device="cuda:0", max_end_silence_time=500)
+    device = torch.device(f"cuda:{rank%num_gpus}")
+    model = AutoModel(model="fsmn-vad", model_revision="v2.0.4", device=f"cuda:{rank%num_gpus}", max_end_silence_time=500)
     # model.to(device)
 
     done_tasks = []
@@ -95,8 +143,11 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--streaming", action="store_true")
     parser.add_argument("--max-duration", type = float, default=30)
+    parser.add_argument("--merge-duration", type = float, default=10)
+    parser.add_argument("--min-duration", type = float, default=2)
+    parser.add_argument("--max-blank-duration", type = float, default=5)
     parser.add_argument("--task-dir", type=str)
-    parser.add_argument("--world-size", type=int, default=1)
+    parser.add_argument("--world-size", type=int, default=16)
     parser.add_argument("--save-dir", type=str)
     parser.add_argument("--done-update-interval", type=int, default=100)
 
